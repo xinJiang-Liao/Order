1c818212cb23a845831979ed824827e6cf4782a6
廖江鑫 <1069672898@qq.com>
2022-02-14 15:46:16 +0800

    chore(Sequelize):Trying to connect to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule as Sequelize } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    Sequelize.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'liaojiangxin',
+      password: '123456789qq',
+      database: 'test',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
